c296c7e4eb4ff767586ad2cd7a1d184ce628213f
Juyeon Han <jennybe0117@gmail.com>
2022-11-09 20:48:08 +0900

    feat: 주석, 변수 업데이트

--- widget/public/brandpay.js
+++ widget/public/brandpay.js
@@ -1,23 +1,28 @@
-const clientKey = 'test_ck_D5GePWvyJnrK0W0k6q8gLzN97Eoq';
-const customerKey = 'tosspayments'; // 상점에서 고객을 구분하기 위해 발급한 고객의 고유 ID로 변경하세요.
+// API 키 설정
+// 문서: https://docs.tosspayments.com/guides/brandpay/integration#api-키-설정-및-sdk-준비
+const clientKey = 'test_ck_D5GePWvyJnrK0W0k6q8gLzN97Eoq'; // 테스트용 클라이언트 키입니다. 계정 설정이 진행된 후에는 내 상점의 키 값으로 변경하세요.
+const customerKey = 'CUSTOMER_KEY'; // 상점에서 고객을 구분하기 위해 발급한 고객의 고유 ID로 변경하세요.
 
 // brandpay 인스턴스 생성
 const brandpay = BrandPay(clientKey, customerKey, {
+  // 개발자센터에 등록한 `redirectUrl`을 추가하세요.
+  // 문서: https://docs.tosspayments.com/reference/brandpay-sdk#초기화-파라미터
   redirectUrl: window.location.origin + '/callback-auth',
 });
 
 const paymentParams = {
-  orderId: 'order123', // 주문에 대한 고유한 ID 값
+  orderId: 'ORDER_ID', // 주문에 대한 고유한 ID 값
   orderName: '생수 외 1건', // 결제에 대한 주문명
   successUrl: window.location.origin + '/success',
   failUrl: window.location.origin + '/fail',
 };
 
-
 let brandpayWidget;
 
 initialize();
-document.querySelector('#payment-form').addEventListener('submit', handleSubmit);
+document
+  .querySelector('#payment-form')
+  .addEventListener('submit', handleSubmit);
 
 async function initialize() {
   // brandpayWidget 인스턴스 생성
