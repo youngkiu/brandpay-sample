0fd8235ad718569d340301e0a6acf3265df00808
Juyeon Han <jennybehan@users.noreply.github.com>
2022-11-17 18:03:33 +0900

    Update widget/public/brandpay.js
    Co-authored-by: 김하영 <91514634+hayoung-kim-toss@users.noreply.github.com>

--- widget/public/brandpay.js
+++ widget/public/brandpay.js
@@ -29,7 +29,7 @@ async function initialize() {
     ui: {
       promotionSection: {
         summary: {
-          visible: false,
+          visible: true,
         },
         description: {
           visible: true,
