fe3b95f8945ff65ad1cadc9b2d6edf38d7e9a2e9
Juyeon Han <jennybe0117@gmail.com>
2022-11-17 17:06:14 +0900

    feat: 샘플 업데이트

--- widget/public/brandpay.js
+++ widget/public/brandpay.js
@@ -1,14 +1,15 @@
 // API 키 설정
 // 문서: https://docs.tosspayments.com/guides/brandpay/integration#api-키-설정-및-sdk-준비
-const clientKey = 'test_ck_D5GePWvyJnrK0W0k6q8gLzN97Eoq'; // 테스트용 클라이언트 키입니다. 계정 설정이 진행된 후에는 내 상점의 키 값으로 변경하세요.
+const clientKey = 'test_ck_D5GePWvyJnrK0W0k6q8gLzN97Eoq';
 const customerKey = 'CUSTOMER_KEY'; // 상점에서 고객을 구분하기 위해 발급한 고객의 고유 ID로 변경하세요.
 
-// brandpay 인스턴스 생성
+// brandpay 초기화
 const brandpay = BrandPay(clientKey, customerKey, {
-  // 개발자센터에 등록한 `redirectUrl`을 추가하세요.
-  // 문서: https://docs.tosspayments.com/reference/brandpay-sdk#초기화-파라미터
   redirectUrl: window.location.origin + '/callback-auth',
 });
+
+// 결제 위젯 객체
+let paymentMethodsWidget = null;
 
 const paymentParams = {
   orderId: 'ORDER_ID', // 주문에 대한 고유한 ID 값
@@ -17,29 +18,18 @@ const paymentParams = {
   failUrl: window.location.origin + '/fail',
 };
 
-let brandpayWidget;
-
 initialize();
-document
-  .querySelector('#payment-form')
-  .addEventListener('submit', handleSubmit);
 
 async function initialize() {
-  // brandpayWidget 인스턴스 생성
-  brandpayWidget = brandpay.widget({ amount: 1000 });
+  // 결제 수단 위젯 객체 초기화
+  paymentMethodsWidget = brandpay.createPaymentMethodsWidget({ amount: 1000 });
 
-  // 결제 수단을 렌더링 할 수 있는 위젯 생성
-  const paymentMethods = brandpayWidget.paymentMethods('payment');
-
-  // 결제 수단 렌더링
-  paymentMethods.render('#payment-methods-widget', {
-    // methodType: '카드', // 위젯에서 보여줄 수 있는 결제 수단 선택
-    // methodId: 'METHOD_ID', // 위젯에서 기본 결제 수단으로 선택할 결제 수단 ID
+  // 결제 수단 위젯 렌더
+  paymentMethodsWidget.render('#payment-methods-widget', {
     ui: {
-      // 위젯 UI에서 변경할 수 있는 옵션
       promotionSection: {
         summary: {
-          visible: true,
+          visible: false,
         },
         description: {
           visible: true,
@@ -50,12 +40,27 @@ async function initialize() {
   });
 }
 
-// 결제하기
+document
+  .querySelector('#payment-form')
+  .addEventListener('submit', handleSubmit);
+
+document.querySelector('#button').addEventListener('click', updateAmount);
+
+// 결제 금액 업데이트
+async function updateAmount(e) {
+  e.preventDefault();
+  paymentMethodsWidget.updateAmount(50000);
+}
+
+// 결제 하기
 async function handleSubmit(e) {
   e.preventDefault();
 
+  // 위젯 결제 정보
+  const widgetPaymentParams = paymentMethodsWidget.getPaymentParams();
+
   await brandpay.requestPayment({
-    widget,
     ...paymentParams,
+    ...widgetPaymentParams,
   });
 }
